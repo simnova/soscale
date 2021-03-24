8b9e600f66632c34c5f90fa987ada7871ee880c5
Patrick Gidich <patrick@simnova.com>
2021-03-24 14:09:47 -0400

    cors

--- data-access/graphql/index.ts
+++ data-access/graphql/index.ts
@@ -1,4 +1,5 @@
 import { ApolloServer, gql }  from 'apollo-server-azure-functions';
+import { HttpRequest, Context } from "@azure/functions";
 
 // Construct a schema, using GraphQL schema language
 const typeDefs = gql`
@@ -22,4 +23,27 @@ const server = new ApolloServer(
   },
 );
 
-exports.graphqlHandler = server.createHandler();
+const graphqlHandler = (server, context, req) => {
+  const graphqlHandlerObj = server.createHandler({
+    cors: {
+      origin: "*",
+      preflightContinue: false, 
+      credentials: true,
+    },
+  })
+
+  // https://github.com/Azure/azure-functions-host/issues/6013
+  req.headers["x-ms-privatelink-id"] = ""
+  // apollo-server only reads this specific string
+  req.headers["Access-Control-Request-Headers"] =
+    req.headers["Access-Control-Request-Headers"] ||
+    req.headers["access-control-request-headers"]
+  return graphqlHandlerObj(context, req)
+}
+
+export default (context: Context, req: HttpRequest) => {
+  return graphqlHandler(server, context, req);
+};
+
+
+//exports.graphqlHandler = graphqlHandler(server,context,req);
